8d022fb960fb73227e950492b48ab26739b5cedf
Gerardo Figueroa <gerardofigueroa375@gmail.com>
2025-10-20 19:53:09 -0600

    Segundo commit, creado el controlador de acceso y plantilla base de acceso y vista de acceso

---
+++ app/Http/Controllers/Auth/AccesoController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Iluminate\Support\Facades\Auth;
+use Iluminate\Support\Facades\Validator;
+
+class AccesoController extends Controller
+{
+    public function mostrarFormulario(){
+        return view('auth.acceso');
+    }
+    public function iniciarSesion(Request $request){
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if( $validator->fails() ){
+            if( $request->ajax() ){
+                return response()->json([
+                    'success' => false,
+                    'errors' => $validator->errors()
+                ], 422);
+            }
+
+            return redirect()
+            ->back()
+            ->withErrors( $validator )
+            ->withInput($request->only('email') );
+        }
+
+        $credenciales = $request->only('email', 'password');
+
+        if( Auth::attempt( $credenciales ) ){
+            $request->session()->regenerate();
+            $usuario = Auth::user();
+
+            if( $request->ajax() ){
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Inicio de sesion exitoso',
+                    'redirect' => ''
+                ]);
+            }
+        }
+
+    }
+}
